dcd4624ec86aeefedb614c3bd404350400ace3f7
tacmac <tacuma.igei@gmail.com>
2017-05-13 02:05:34 +0900

    add Hello world

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron');
+ 
+// window objectがGCされないようにするために、globalに定義する
+let win;
+ 
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600});
+ 
+  win.loadURL(`file://${__dirname}/index.html`);
+ 
+  win.on('closed', () => {
+    // windowがクローズされたら null にして削除
+    win = null;
+  });
+}
+ 
+app.on('ready', createWindow);
+ 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+ 
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
